1341c8ce4b5172d033e3a2b500e81ebd44e1c80d
Rachit Bhatta <rachitbhatta2009@gmail.com>
2025-09-02 16:27:42 +0545

    Utility code added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./DataBase/index.database.js";
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT||8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Database connection failed:", error);
+    });
