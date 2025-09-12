e077c0f46afefeea142119594cb7505e5de89a2a
Rachit Bhatta <rachitbhatta2009@gmail.com>
2025-09-12 17:50:45 +0545

    Add watch history retrieval feature to user controller

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -318,16 +318,62 @@ const getChannelProfile=asyncHandler(async(req,res,next)=>{
     .status(200)
     .json(new ApiResponse(200,channel[0],"Channel profile fetched successfully"))
 });
+const getWatchHistory=asyncHandler(async(req,res,next)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },{
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"WatchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1,
 
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner",
+
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user[0]?.watchHistory||[],"Watch history fetched successfully"));
+});
 export {
-    registerUser
-    ,loginUser
-    ,userLogOut
-    ,accessRefreshToken
-    ,changePassword
-    ,getCurrentUser
-    ,updateAccountDetails
-    ,updateUserAvatar,
+    registerUser,
+    loginUser,
+    userLogOut,
+    accessRefreshToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
     getChannelProfile,
-
+    getWatchHistory
     };
